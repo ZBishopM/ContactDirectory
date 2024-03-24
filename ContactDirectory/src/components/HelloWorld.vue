769478093dea63c23967d2d12ac4fb0a12582f70
ZBishop <obisporicapa@gmail.com>
2024-03-24 17:40:15 -0500

    adding sass

--- ContactDirectory/src/components/HelloWorld.vue
+++ ContactDirectory/src/components/HelloWorld.vue
@@ -8,12 +8,12 @@ const count = ref(0)
 
 <template>
   <h1>{{ msg }}</h1>
-
+  
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      Este es un p
+      <code> y esta es una etiqueta code</code>
     </p>
   </div>
 
@@ -30,9 +30,3 @@ const count = ref(0)
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
