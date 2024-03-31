267104410faf24b8d22986a766e9b96e0adf0ed6
ZBishop <obisporicapa@gmail.com>
2024-03-31 16:08:27 -0500

    using theme from system and fix for maz-ui phone component

--- ContactDirectory/src/App.vue
+++ ContactDirectory/src/App.vue
@@ -1,15 +1,13 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { polyfillCountryFlagEmojis } from 'country-flag-emoji-polyfill';
+import { useThemeHandler} from 'maz-ui'
+polyfillCountryFlagEmojis();
+useThemeHandler({storageThemeKey: 'system'});
 </script>
 
 <template>
+  <h1>Contact.me</h1>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <router-view/>
 </template>
