82d26f8ff62906840bbef1bbe86e8361a1ce5971
ZBishop <obisporicapa@gmail.com>
2024-03-31 16:05:04 -0500

    Because of using maz-ui and unpluginvuecomponents

--- ContactDirectory/vite.config.ts
+++ ContactDirectory/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { UnpluginVueComponentsResolver } from 'maz-ui/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      dts: true,
+      resolvers: [UnpluginVueComponentsResolver()],
+    }),
+  ],
 })
